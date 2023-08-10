b4e0afad0c3a5fc0aea460d7bc631c73beec3ea9
ramyasreerepoo <ramya64.sree@gmail.com>
2023-08-10 17:32:55 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is my React Poc</h1>
+        <p>Trying to commit the code to Github & call in different project</p>
+      </div>
+    );
+  }
+}
+
+export default App;
