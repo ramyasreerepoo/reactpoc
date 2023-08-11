45e873e976c3bb7995f86f1ab043a8413a226b54
ramyasreerepoo <ramya64.sree@gmail.com>
2023-08-12 04:10:26 +0530

    config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      ['@babel/preset-react', {targets: {node: 'current'}}] // add this
+  presets: [
+    isTestEnv && [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    (isProductionEnv || isDevelopmentEnv) && [
+      '@babel/preset-env',
+      {
+        forceAllTransforms: true,
+        useBuiltIns: 'usage',
+        corejs: '3',
+        modules: false,
+        exclude: ['transform-typeof-symbol']
+      }
+    ],
+    [
+      '@babel/preset-react',
+      {
+        development: isDevelopmentEnv || isTestEnv,
+        useBuiltIns: true
+      }
     ]
+  ].filter(Boolean)
   };
